f37d62632fc6e9807f4f85dbfd535d83a8686e0b
unknown <nesad.omeragich27@gmail.com>
2020-04-22 00:29:19 +0200

    added date to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './header.css';
+import style from './header.module.css';
 
 const Header = () => {
+  const date = new Date();
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
   return (
-    <div className='header'>
-      <header></header>
+    <div className={style.header}>
+      <header>
+        <span className={style.dateTime}>
+          {date.toLocaleDateString('en-UK', options)}
+        </span>
+      </header>
     </div>
   );
 };

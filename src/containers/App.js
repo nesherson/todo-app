3c06c1c4d39ffdf00177ded366ce632a5b08ace2
unknown <nesad.omeragich27@gmail.com>
2020-06-29 01:29:35 +0200

    added option for section addiotion

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     listOfSections: [
       {
         sectionName: 'Inbox',
-        body: [
+        listOfTasks: [
           {
             id: 3,
             body: 'Inbox default task 1',
@@ -30,7 +30,7 @@ class App extends Component {
       },
       {
         sectionName: 'Inbox 2',
-        body: [
+        listOfTasks: [
           {
             id: 3,
             body: 'Inbox2 default task 1',
@@ -45,12 +45,18 @@ class App extends Component {
       },
     ],
     input: '',
+    sectionNameInput: '',
     currentSelected: 0,
   };
 
   handleInput = (event) => {
     const text = event.target.value;
     this.setState({ input: text });
+  };
+
+  handleSectionNameInput = (e) => {
+    const text = e.target.value;
+    this.setState({ sectionNameInput: text });
   };
 
   handleNewTask = (event) => {
@@ -64,51 +70,64 @@ class App extends Component {
       };
 
       const tempSectionList = [...this.state.listOfSections];
-      tempSectionList[this.state.currentSelected].body.push(task);
+      tempSectionList[this.state.currentSelected].listOfTasks.push(task);
 
       this.setState({ listOfSections: tempSectionList });
       this.setState({ input: '' });
     }
   };
 
+  handleNewSection = (e) => {
+    e.preventDefault();
+    const input = this.state.sectionNameInput;
+    const section = {
+      sectionName: input,
+      listOfTasks: [],
+    };
+    const tempSectionList = [...this.state.listOfSections];
+    tempSectionList.push(section);
+
+    this.setState({ listOfSections: tempSectionList });
+  };
+
   handleCompleteTask = (index) => {
     const list = [...this.state.listOfSections];
     const currentSelected = this.state.currentSelected;
 
-    list[currentSelected].body[index].completed = !list[currentSelected].body[
-      index
-    ].completed;
+    list[currentSelected].listOfTasks[index].completed = !list[currentSelected]
+      .listOfTasks[index].completed;
     this.setState({ listOfSections: list });
   };
 
   handleRemoveTask = (index) => {
-    const list = [...this.state.listOfTasks];
-    list.splice(index, 1);
-    this.setState({ listOfTasks: list });
+    const list = [...this.state.listOfSections];
+    const currentSelected = this.state.currentSelected;
+    list[currentSelected].listOfTasks.splice(index, 1);
+    this.setState({ listOfSections: list });
   };
 
   handleSectionClick = (i) => {
     this.setState({
-      listOfTasks: [...this.state.listOfSections[i].body],
+      listOfTasks: [...this.state.listOfSections[i].listOfTasks],
       currentSelected: i,
     });
   };
 
   render() {
     const currentSelected = this.state.currentSelected;
-    const listOfTasks = this.state.listOfSections[currentSelected].body.map(
-      (task, index) => {
-        return (
-          <Task
-            value={task.body}
-            key={task.id}
-            completed={task.completed}
-            onClick={() => this.handleCompleteTask(index)}
-            onDoubleClick={() => this.handleRemoveTask(index)}
-          />
-        );
-      }
-    );
+    const listOfTasks = this.state.listOfSections[
+      currentSelected
+    ].listOfTasks.map((task, index) => {
+      return (
+        <Task
+          value={task.body}
+          key={task.id}
+          completed={task.completed}
+          onClick={() => this.handleCompleteTask(index)}
+          onDoubleClick={() => this.handleRemoveTask(index)}
+        />
+      );
+    });
 
     console.log(this.state.listOfSections[this.state.currentSelected]);
 
@@ -127,7 +146,15 @@ class App extends Component {
         <Header />
         <div className='main'>
           <div className='container'>
-            <Sidebar>{listOfSections}</Sidebar>
+            <Sidebar>
+              {listOfSections}
+              <NewTask
+                value={this.state.sectionNameInput}
+                onChange={this.handleSectionNameInput}
+                onSubmit={this.handleNewSection}
+                onClick={this.handleNewSection}
+              />
+            </Sidebar>
             <ListOfTasks
               name={this.state.listOfSections[currentSelected].sectionName}
             >
